bd2cd7af591fc02ad2e863248f24eb0837c5b221
정세현 <sehyun>
2024-12-30 12:49:38 +0900

    aaaa

--- Git_Test_IMS/src/main/java/com/itwillbs/gittestims/IMSController.java
+++ Git_Test_IMS/src/main/java/com/itwillbs/gittestims/IMSController.java
@@ -3,10 +3,6 @@ package com.itwillbs.gittestims;
 public class IMSController {
 	// 주석 추가 - 임한섭
 	// 주석 한 번 더 추가
-<<<<<<< HEAD
 	// 주석 추가 정세현
-=======
-
->>>>>>> branch 'main' of https://github.com/hansub-im/Git_IMS
 }
 // 주석 - 허진용
